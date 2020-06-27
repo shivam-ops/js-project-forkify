68629fdcde7897bcb2e0d78790c704cc4cbad2ea
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-06-27 14:46:26 +0530

    adding a loader

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,6 +1,27 @@
 export const elements = {
     searchForm : document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
+    searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list')
 
+};
+
+export const elememtStrings = {
+    loader: 'loader'
+}
+
+export const renderLoader = parent => {
+    const loader = `
+        <div class="${elememtStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elememtStrings.loader}`);
+    if(loader) loader.parentElement.removeChild(loader);
 };
