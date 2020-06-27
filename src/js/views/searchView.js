9e9611b6907c850e27c29fd0b9926404b9d148db
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-06-27 23:27:34 +0530

    implementing search results pagination

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -9,6 +9,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 };
 
 const limitRecipeTitle = (title, limit = 17) => {
@@ -47,6 +48,44 @@ const renderRecipe = recipe => {
 }
 
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+const createButton = (page, type) => `
+                <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+                <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+                <svg class="search__icon">
+                    <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+                </svg>
+                
+                </button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
+    let button;
+    
+    if(page === 1 && pages > 1) {
+        //Only Button to go to next page
+        button = createButton(page, 'next');
+    } else if (page < pages) {
+        //Both buttons
+        button = `${createButton(page, 'prev')} ${createButton(page, 'next')}`;
+    }
+    else if (page === pages && pages > 1) {
+        //Only button to go to prev page
+        button = createButton(page, 'prev')
+    }
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
+
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    // render results of current 
+    const start = (page - 1) * resPerPage;
+    const end =page * resPerPage;
+
+    recipes.slice(start , end).forEach(renderRecipe);
+
+    // render pagination button
+    renderButtons(page, recipes.length, resPerPage);
 };
