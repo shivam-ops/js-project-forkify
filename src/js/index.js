b38705d8642d70ea1ecdf91a93582a4a7f65fe6e
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-06-28 15:30:11 +0530

    building the recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe'
 import * as searchView from './views/searchView'
 import {elements, renderLoader, clearLoader} from './views/base';
 
@@ -9,6 +10,10 @@ import {elements, renderLoader, clearLoader} from './views/base';
  * - Liked recipes
  */
 const state = {};
+
+/*
+ * SEARCH CONTROLLER
+ */
 
 const controlSearch = async () => {
     //1) Get query from the view
@@ -48,3 +53,10 @@ elements.searchResPages.addEventListener('click', e => {
     }
 })
 
+/*
+ * RECIPE CONTROLLER
+ */
+
+ const r = new Recipe(49346);
+ r.getRecipe();
+ console.log(r);
