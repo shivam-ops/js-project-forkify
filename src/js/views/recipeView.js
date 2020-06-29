2d86365b1c032198b3390a382e1aaa4be83f4133
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-06-29 23:59:48 +0530

    updating recipe servings

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -7,22 +7,23 @@ export const clearRecipe = () => {
 }
 
 const formatCount = count => {
-    if(count) {
-        // count = 2.5--> 2 1/2
+    if (count) {
+        // count = 2.5 --> 5/2 --> 2 1/2
+        // count = 0.5 --> 1/2
+        const newCount = Math.round(count * 10000) / 10000;
+        const [int, dec] = newCount.toString().split('.').map(el => parseInt(el, 10));
 
-        const [int, dec] = count.toString().split('.').map(el => parseInt(el, 10));
+        if (!dec) return newCount;
 
-        if(!dec) return count;
-
-        if(int === 0) {
-            const fr = new Fraction(count);
+        if (int === 0) {
+            const fr = new Fraction(newCount);
             return `${fr.numerator}/${fr.denominator}`;
         } else {
-            const fr = new Fraction(count - int);
+            const fr = new Fraction(newCount - int);
             return `${int} ${fr.numerator}/${fr.denominator}`;
         }
     }
-    return `?`;
+    return '?';
 };
 
 const createIngredient = ingredient => `
@@ -40,91 +41,81 @@ const createIngredient = ingredient => `
 
 export const renderRecipe = recipe => {
     const markup = `
-    
-    <figure class="recipe__fig">
-        <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
-        <h1 class="recipe__title">
-            <span>${recipe.title}</span>
-        </h1>
-    </figure>
-    <div class="recipe__details">
-        <div class="recipe__info">
-            <svg class="recipe__info-icon">
-                <use href="img/icons.svg#icon-stopwatch"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${recipe.time}</span>
-            <span class="recipe__info-text"> minutes</span>
-        </div>
-        <div class="recipe__info">
-            <svg class="recipe__info-icon">
-                <use href="img/icons.svg#icon-man"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
-            <span class="recipe__info-text"> servings</span>
-
-            <div class="recipe__info-buttons">
-                <button class="btn-tiny">
-                    <svg>
-                        <use href="img/icons.svg#icon-circle-with-minus"></use>
-                    </svg>
-                </button>
-                <button class="btn-tiny">
-                    <svg>
-                        <use href="img/icons.svg#icon-circle-with-plus"></use>
-                    </svg>
-                </button>
-            </div>
-
-        </div>
-        <button class="recipe__love">
-            <svg class="header__likes">
-                <use href="img/icons.svg#icon-heart-outlined"></use>
-            </svg>
-        </button>
-    </div>
-
-
-
-    <div class="recipe__ingredients">
-        <ul class="recipe__ingredient-list">
-        ${recipe.ingredients.map(el => createIngredient(el)).join('')}
-
-
-            <li class="recipe__item">
-                <svg class="recipe__icon">
-                    <use href="img/icons.svg#icon-check"></use>
+        <figure class="recipe__fig">
+            <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
+            <h1 class="recipe__title">
+                <span>${recipe.title}</span>
+            </h1>
+        </figure>
+        <div class="recipe__details">
+            <div class="recipe__info">
+                <svg class="recipe__info-icon">
+                    <use href="img/icons.svg#icon-stopwatch"></use>
                 </svg>
-                <div class="recipe__count">1000</div>
-                <div class="recipe__ingredient">
-                    <span class="recipe__unit">g</span>
-                    pasta
+                <span class="recipe__info-data recipe__info-data--minutes">${recipe.time}</span>
+                <span class="recipe__info-text"> minutes</span>
+            </div>
+            <div class="recipe__info">
+                <svg class="recipe__info-icon">
+                    <use href="img/icons.svg#icon-man"></use>
+                </svg>
+                <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+                <span class="recipe__info-text"> servings</span>
+                <div class="recipe__info-buttons">
+                    <button class="btn-tiny btn-decrease">
+                        <svg>
+                            <use href="img/icons.svg#icon-circle-with-minus"></use>
+                        </svg>
+                    </button>
+                    <button class="btn-tiny btn-increase">
+                        <svg>
+                            <use href="img/icons.svg#icon-circle-with-plus"></use>
+                        </svg>
+                    </button>
                 </div>
-            </li>
-        </ul>
-
-        <button class="btn-small recipe__btn">
-            <svg class="search__icon">
-                <use href="img/icons.svg#icon-shopping-cart"></use>
-            </svg>
-            <span>Add to shopping list</span>
-        </button>
-    </div>
-
-    <div class="recipe__directions">
-        <h2 class="heading-2">How to cook it</h2>
-        <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__by">${recipe.author}</span>. Please check out directions at their website.
-        </p>
-        <a class="btn-small recipe__btn" href="${recipe.url}" target="_blank">
-            <span>Directions</span>
-            <svg class="search__icon">
-                <use href="img/icons.svg#icon-triangle-right"></use>
-            </svg>
-
-        </a>
-    </div>
-    
+            </div>
+            <button class="recipe__love">
+                <svg class="header__likes">
+                    <use href="img/icons.svg#icon-heart-outlined"></use>
+                </svg>
+            </button>
+        </div>
+        <div class="recipe__ingredients">
+            <ul class="recipe__ingredient-list">
+                ${recipe.ingredients.map(el => createIngredient(el)).join('')}
+            </ul>
+            <button class="btn-small recipe__btn recipe__btn--add">
+                <svg class="search__icon">
+                    <use href="img/icons.svg#icon-shopping-cart"></use>
+                </svg>
+                <span>Add to shopping list</span>
+            </button>
+        </div>
+        <div class="recipe__directions">
+            <h2 class="heading-2">How to cook it</h2>
+            <p class="recipe__directions-text">
+                This recipe was carefully designed and tested by
+                <span class="recipe__by">${recipe.author}</span>. Please check out directions at their website.
+            </p>
+            <a class="btn-small recipe__btn" href="${recipe.url}" target="_blank">
+                <span>Directions</span>
+                <svg class="search__icon">
+                    <use href="img/icons.svg#icon-triangle-right"></use>
+                </svg>
+            </a>
+        </div>
     `;
-    elements.recipe.insertAdjacentHTML('afterbegin', markup); 
+    elements.recipe.insertAdjacentHTML('afterbegin', markup);
+};
+
+
+export const updateServingsIngredients = recipe => {
+    // Update servings
+    document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+
+    // Update ingredeints
+    const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+    countElements.forEach((el, i) => {
+        el.textContent = formatCount(recipe.ingredients[i].count);
+    });
 };
