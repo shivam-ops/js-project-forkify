02c3f9c2dddd29a2b65df26b7c741aa6cb273738
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-06-29 08:26:33 +0530

    rendering recipes

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -33,9 +33,9 @@ export default class Recipe {
     }
 
     parseIngredients() {
-
-        const unitsLong = ['tablesnpoons', 'tablesnpoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups','pounds'];
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
         const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitsShort, 'kg', 'g'];
 
         const newIngredients = this.ingredients.map(el => {
             // 1) Uniform units
@@ -43,12 +43,13 @@ export default class Recipe {
             unitsLong.forEach((unit, i) => {
                 ingredient = ingredient.replace(unit, unitsShort[i]);
             });
-            // 2) Remove parenthesis
+
+            // 2) Remove parentheses
             ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
 
-            // 3) Parse ingredients into count, unit and ingredients
+            // 3) Parse ingredients into count, unit and ingredient
             const arrIng = ingredient.split(' ');
-            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
             let objIng;
             if (unitIndex > -1) {
@@ -86,10 +87,8 @@ export default class Recipe {
                 }
             }
 
-
             return objIng;
         });
-
         this.ingredients = newIngredients;
     }
 }
